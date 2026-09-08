a41a74dd1873a4f1b2bc92154944881004100d9e
Аида Ерболат <aidaerbolat@Aidas-MacBook-Pro.local>
2026-09-09 01:01:20 +0500

    Replace magic values with named constants

--- src/ServerDirector.java
+++ src/ServerDirector.java
@@ -1,20 +1,30 @@
 public class ServerDirector {
 
+    private static final String OPERATING_SYSTEM = "Ubuntu";
+
+    private static final String DEVELOPMENT_HOSTNAME = "dev-server";
+    private static final int DEVELOPMENT_CPU_CORES = 2;
+    private static final int DEVELOPMENT_RAM_GB = 4;
+
+    private static final String PRODUCTION_HOSTNAME = "prod-server";
+    private static final int PRODUCTION_CPU_CORES = 8;
+    private static final int PRODUCTION_RAM_GB = 32;
+
     public void makeDevelopmentServer(ServerBuilder builder) {
         builder
-                .hostname("dev-server")
-                .operatingSystem("Ubuntu")
-                .cpuCores(2)
-                .ramGb(4)
+                .hostname(DEVELOPMENT_HOSTNAME)
+                .operatingSystem(OPERATING_SYSTEM)
+                .cpuCores(DEVELOPMENT_CPU_CORES)
+                .ramGb(DEVELOPMENT_RAM_GB)
                 .enableDocker();
     }
 
     public void makeProductionServer(ServerBuilder builder) {
         builder
-                .hostname("prod-server")
-                .operatingSystem("Ubuntu")
-                .cpuCores(8)
-                .ramGb(32)
+                .hostname(PRODUCTION_HOSTNAME)
+                .operatingSystem(OPERATING_SYSTEM)
+                .cpuCores(PRODUCTION_CPU_CORES)
+                .ramGb(PRODUCTION_RAM_GB)
                 .enableDocker()
                 .enableMonitoring();
     }
